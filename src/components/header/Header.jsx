7189f3a82374be031cca90d5cc64c2574801e633
faris <faris@mail.com>
2021-12-07 22:24:54 +0500

    :sparkles: make header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,19 @@ import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import { ContactLinks } from "./ContactLinks";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 export const Header = (props) => {
+  const match = useMediaQuery("(max-width:920px)");
+  const title = match
+    ? "GCD No.1 DIK"
+    : "Government Degree College No.1 Dera Ismail Khan";
   return (
-    <header className="bg-dark text-white">
+    <header>
       <Navbar bg="light">
         <Container fluid>
           <Navbar.Brand as={Link} to="/">
-            Government Degree College No.1 Dera Ismail Khan
+            {title}
           </Navbar.Brand>
           <ContactLinks />
         </Container>
